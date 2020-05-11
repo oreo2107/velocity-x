0c1ac3bdd5416b32a413aad6a0b41e1a718b92fc
Om <ommishra32868@gmail.com>
2020-05-11 19:37:13 +0530

    Implemented FishAction interface

--- Kotlin-Bootcamp/src/com/firstPackage/AquariumFish.kt
+++ Kotlin-Bootcamp/src/com/firstPackage/AquariumFish.kt
@@ -5,7 +5,9 @@ fun makeFish(){
     val plecostomus = Plecostomus()
 
     println("Shark: ${shark.color}")
+    shark.eat()
     println("Plecostomus: ${plecostomus.color}")
+    plecostomus.eat()
 }
 fun main(){
     makeFish()
--- Kotlin-Bootcamp/src/com/firstPackage/AquariumFishes.kt
+++ Kotlin-Bootcamp/src/com/firstPackage/AquariumFishes.kt
@@ -1,11 +1,19 @@
 package com.firstPackage
-
+interface FishAction{
+    fun eat()
+}
 abstract class AquariumFishes {
     abstract val color: String
 }
-class Shark: AquariumFishes(){
+class Shark: AquariumFishes(),FishAction{
     override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
 }
-class Plecostomus: AquariumFishes(){
+class Plecostomus: AquariumFishes(),FishAction{
     override val color = "gold"
+    override fun eat() {
+        println("eat algae")
+    }
 }
