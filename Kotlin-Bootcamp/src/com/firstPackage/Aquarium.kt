fa50b4d6336eaf37b2e5aba0d79bcad77bce6d82
Om <ommishra32868@gmail.com>
2020-05-11 18:50:07 +0530

    Used private visibility modifier for setter method

--- Kotlin-Bootcamp/src/com/firstPackage/Aquarium.kt
+++ Kotlin-Bootcamp/src/com/firstPackage/Aquarium.kt
@@ -6,7 +6,8 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
     }
     var volume: Int
     get() = width*height*length/1000
-    set(value){
+    //private getter allows only reading of data outside the file
+    private set(value){
         height = (value * 1000) / (width * length)
     }
     constructor(numberOfFish: Int): this(){
